5ba84b69d35eedbec4f4e1f4912f6cd31d7fffbf
Raul Sanchez <raulsanchez1024@gmail.com>
2021-11-11 15:35:24 -0700

    Starts lint/format process.

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+const rules = {
+  'func-names': 'off',
+  'react-hooks/rules-of-hooks': 'error',
+  'react-hooks/require-default-props': 'off',
+  'react-hooks/jsx-props-no-spreading': 'off',
+  'react/react-in-jsx-scope': 'off',
+  'react/button-has-type': 'off',
+  'react/function-component-definition': 'off',
+  'import/prefer-default-export': 'off',
+  'no-param-reassign': [
+    'error',
+    { props: true, ignorePropertyModificationsFor: ['state'] },
+  ],
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'react-hooks', 'prettier'],
+  rules,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: {
+          ecmaVersion: 2019,
+          sourceType: 'module',
+          jsx: true,
+        },
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+      ],
+      rules: {
+        ...rules,
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: ['variable'],
+            format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+            leadingUnderscore: 'allow',
+          },
+        ],
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'import/no-default-export': 'error',
+      },
+    },
+  ],
+};
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -9,15 +9,18 @@ import './Home.css';
 export function Home() {
   // Hooks
   const dispatch = useDispatch();
-  const counter = useAppSelector(state => getCounter(state));
+  const counter = useAppSelector((state) => getCounter(state));
 
   return (
     <div className="home__container">
       <h1>Home</h1>
-      <p>counter: {counter}</p>
+      <p>
+        counter:
+        {counter}
+      </p>
       <div>
-        <button onClick={e => dispatch(setCounter(counter + 1))}>+</button>
-        <button onClick={e => dispatch(setCounter(counter - 1))}>-</button>
+        <button onClick={() => dispatch(setCounter(counter + 1))}>+</button>
+        <button onClick={() => dispatch(setCounter(counter - 1))}>-</button>
       </div>
     </div>
   );
